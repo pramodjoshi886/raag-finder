113e1fd41b047abfcf40845aacddc367d3ba541b
Ramprasad <r.raghunath@tu-bs.de>
2024-09-10 19:59:27 +0200

    Add more ragas to the library

--- src/components/Raagas.js
+++ src/components/Raagas.js
@@ -1,7 +1,17 @@
 const Raagas = [
-    { name: 'Bilawal', aaroha: 'S,R,G,m,P,D,N,S', avaroh: 'S,N,D,P,m,G,R,S' },
-    { name: 'Yaman', aaroha: 'S,R,G,M,P,D,N,S', avaroh: 'S,N,D,P,M,G,R,S' },
-    { name: 'Bhoopali', aaroha: 'S,R,G,P,D,S', avaroh: 'S,D,P,G,R,S' },
+    { label: 'Bilawal', aaroha: 'S,R,G,m,P,D,N,S', avaroh: 'S,N,D,P,m,G,R,S' },
+    { label: 'Yaman', aaroha: 'S,R,G,M,P,D,N,S', avaroh: 'S,N,D,P,M,G,R,S' },
+    { label: 'Bhoopali', aaroha: 'S,R,G,P,D,S', avaroh: 'S,D,P,G,R,S' },
+    { label: 'Yaman Kalyan', aaroha: 'S,R,G,M,P,D,N,S', avaroh: 'S,N,D,P,m,G,R,S' },
+    { label: 'Puriya Kalyan', aaroha: 'S,r,G,M,P,D,N,S', avaroh: 'S,N,D,P,M,G,r,S' },
+    { label: 'Malkauns', aaroha: 'S,g,m,d,n,S', avaroh: 'S,n,d,m,g,S' },
+    { label: 'Durga', aaroha: 'S,R,m,P,D,S', avaroh: 'S,D,P,m,R,S' },
+    { label: 'Asavari', aaroha: 'S,R,m,P,d,S', avaroh: 'S,n,d,P,m,g,R,S' },
+    { label: 'Puriya Dhanasri', aaroha: 'S,r,G,M,P,d,N,S', avaroh: 'S,N,d,P,m,G,r,S' },
+    { label: 'Desh', aaroha: 'S,R,m,P,N,S', avaroh: 'S,n,D,P,m,G,R,S' },
+    { label: 'Madhuvanti', aaroha: 'S,g,M,P,N,S', avaroh: 'S,N,D,P,M,g,R,S' },
+    { label: 'Darbari Kanhada', aaroha: 'S,R,g,m,P,d,n,S', avaroh: 'S,n,d,P,m,g,R,S' },
+    { label: 'Hamsadhvani', aaroha: 'S,R,G,P,N,S', avaroh: 'S,N,P,G,R,S' },
     // Add more raagas as needed
 ];
 
