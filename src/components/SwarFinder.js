ad0c57ad64670d570672fe5a048394cbc5b6233a
Ramprasad <r.raghunath@tu-bs.de>
2024-09-10 20:00:32 +0200

    Add autocomplete functionality to search swaras

--- src/components/SwarFinder.js
+++ src/components/SwarFinder.js
@@ -1,42 +1,50 @@
 import React, { useState } from 'react';
-import '../components/SwarFinder.css'
-import raagas from '../components/Raagas'
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import '../components/SwarFinder.css';
+import raagas from '../components/Raagas';
 
-  
-  function SwarFinder() {
-    const [inputValue, setInputValue] = useState('');
-    const [result, setResult] = useState(null);
-  
-    const findRaag = () => {
-      const raag = raagas.find(raag => raag.name.toLowerCase() === inputValue.toLowerCase());
-      if (raag) {
-        setResult(<div>
-            <h3>Aaroha:</h3>
-            <h1>{raag.aaroha}</h1>
-            <h3>Avaroh:</h3>
-            <h1>{raag.avaroh}</h1>
-        </div>);
+const SwarFinder = () => {
+  const [inputValue, setValue] = useState('');
+  const [result, setResult] = useState(null);
+
+  const findRaag = () => {
+    if (inputValue) {
+      setResult(<div>
+        <h3>Aaroha:</h3>
+        <h1>{inputValue.aaroha}</h1>
+        <h3>Avaroh:</h3>
+        <h1>{inputValue.avaroh}</h1>
+      </div>);
     } else {
-        setResult(
-          <h1>Raaga not found!</h1>
-          );
+      setResult(
+        <h1>Raaga not found!</h1>
+      );
     }
-    };
-  
-    return (
-      <div className="raag-name-finder-container"> 
-        <input
-          type="text"
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
-          placeholder="Enter Raaga Name"
-        />
-        <button onClick={findRaag}>Search</button>
-        <div className="result"> 
-          {result}
-        </div>
-      </div>
-    );
-  }
+  };
+  return (
+    <div>
+      <Autocomplete
+        disablePortal
+        options={raagas}
+        id='find-raag-autocomplete'
+        value={inputValue}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          // console.log(newValue);
+        }}
+        fullWidth
+        // getOptionLabel={(option) => option.name}
 
+        renderInput={(params) => <TextField {...params} label="Raagas" />}
+      />
+      <Button onClick={findRaag}>Search</Button>
+      <div className="result">
+        {result}
+      </div>
+    </div>
+
+  );
+}
 export default SwarFinder;
