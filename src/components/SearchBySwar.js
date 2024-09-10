60c922edfcf0d4b07d1e486ed358b2a232ff987a
Ramprasad <r.raghunath@tu-bs.de>
2024-09-10 20:07:54 +0200

    change name to label

--- src/components/SearchBySwar.js
+++ src/components/SearchBySwar.js
@@ -4,20 +4,20 @@ function SearchBySwar(aaroh, avaroh) {
     aaroh.push('S')
     avaroh.push('S')
     console.log(aaroh, ' aaroh')
-    console.log(avaroh,'avaroh')
+    console.log(avaroh, 'avaroh')
     const matchingRaags = raagas.filter(raag => {
-    
-        if(raag.aaroha.toString() === aaroh.toString() && raag.avaroh.toString() === avaroh.toString()){
+
+        if (raag.aaroha.toString() === aaroh.toString() && raag.avaroh.toString() === avaroh.toString()) {
             console.log(raag)
             return raag;
-        }else{
+        } else {
             console.log(raag.aaroha.toString(), aaroh.toString())
             console.log(raag.avaroh.toString(), avaroh.toString())
         }
     });
 
     console.log(matchingRaags, 'matchin chin')
-    return matchingRaags.length &&  matchingRaags[0].name
+    return matchingRaags.length && matchingRaags[0].label
 
 }
 
